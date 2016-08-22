9f424acf38594f18370869bfedbe1494cff49660
nlarso <nlarso@gmail.com>
2016-08-21 20:11:04 -0600

    Add allow_nil to uniqueness validation test

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
-  should validate_uniqueness_of(:email)
+  should validate_uniqueness_of(:email).allow_nil
   should allow_value('test@example.com').for(:email)
   should_not allow_value('this-is-not-an-email-address').for(:email)
 end
