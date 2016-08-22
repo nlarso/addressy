b9d6b4bb05a526e48e7a16aef0742bb2fa407cf6
nlarso <nlarso@gmail.com>
2016-08-21 18:16:59 -0600

    Add ContactFormModal component

--- app/assets/javascripts/components/address_book.js.jsx
+++ app/assets/javascripts/components/address_book.js.jsx
@@ -6,32 +6,73 @@ class AddressBook extends React.Component {
   constructor(props) {
     super(props);
     this.onContactClick = this.onContactClick.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
 
     this.state = {
-      activeContact: {}
+      contact: { name: null },
+      contacts: this.props.contacts,
+      displayedContact: {},
+      errors: {},
     }
   }
 
   onContactClick(contact) {
-    this.setState({activeContact: contact});
+    this.setState({displayedContact: contact});
+  }
+
+  onFormSubmit(contact) {
+    this.setState({errors: null});
+    const params = {
+      contact: _.mapKeys(contact, (value, key) => { return _.snakeCase(key)})
+    }
+    $.ajax({
+      type: 'POST',
+      url: '/contacts.json',
+      data: params,
+      success: (contact) => {
+        const contacts = _.clone(this.state.contacts);
+        contacts.unshift(contact);
+        this.setState({contacts, displayedContact: contact, contact: { name: null }});
+        $('#contact-form-modal').modal('hide')
+      },
+      error: (data) => {
+        if (data.status == 422) {
+          const errors = data.responseJSON.errors
+          this.setState({errors});
+        }
+      }
+    });
   }
 
   render () {
     return (
-      <div className="row">
-        <div className="col-xs-5">
-          <div className="card">
-            <div className="list-group list-group-flush">
-              <ContactsList
-                onContactClick={this.onContactClick}
-                { ... this.props }
-              />
+      <div>
+        <div className="row">
+          <div className="col-xs-5">
+            <div className="card">
+              <div className="card-header text-xs-right">
+                <button className="btn btn-primary btn-sm" data-target="#contact-form-modal" data-toggle="modal">
+                  <i className="fa fa-plus" />
+                  New Contact
+                </button>
+              </div>
+              <div className="list-group list-group-flush">
+                <ContactsList
+                  onContactClick={this.onContactClick}
+                  contacts={this.state.contacts}
+                />
+              </div>
             </div>
           </div>
+          <div className="col-xs-7">
+            <ContactCard contact={this.state.displayedContact} />
+          </div>
         </div>
-        <div className="col-xs-7">
-          <ContactCard contact={this.state.activeContact} />
-        </div>
+        <ContactFormModal
+          contact={this.state.contact}
+          errors={this.state.errors}
+          onFormSubmit={this.onFormSubmit}
+        />
       </div>
     );
   }
---
+++ app/assets/javascripts/components/contact_form_modal.js.jsx
@@ -0,0 +1,170 @@
+const propTypes = {
+  contact: React.PropTypes.object,
+  errors: React.PropTypes.array,
+  onFormSubmit: React.PropTypes.func
+};
+
+class ContactFormModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      contact: { name: null }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contact } = this.props;
+    if (contact && contact != prevProps.contact) {
+      this.setState({contact});
+    }
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.contact);
+  }
+
+  onInputChange(event) {
+    const { name, value } = event.target;
+    let { contact } = this.state;
+    contact[name] = value;
+    this.setState({contact});
+  }
+
+  renderErrorMessages(name) {
+    const { errors } = this.props;
+
+    if (_.has(errors, name)) {
+      return (
+        <small className="form-control-feedback">{name} {errors[name]}</small>
+      );
+    }
+
+  }
+
+  render () {
+    const { contact } = this.state;
+    const { errors } = this.props;
+    return (
+      <div
+        aria-hidden="true"
+        className="modal fade"
+        data-backdrop="static"
+        id="contact-form-modal"
+        role="dialog"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <div className="col-md-11">
+                <h4 className="modal-title">New Contact</h4>
+              </div>
+              <div className="col-md-1">
+                <button className="close" data-dismiss="modal">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </div>
+            <div className="modal-body">
+              <div className={`form-group ${_.has(errors, 'name') && 'has-danger'}`}>
+                <label>Name</label>
+                <input
+                  name="name"
+                  className="form-control"
+                  value={contact.name || ''}
+                  onChange={this.onInputChange}
+                  placeholder="John Doe"
+                />
+                {this.renderErrorMessages('name')}
+              </div>
+
+              <div className="form-group">
+                <label>Title</label>
+                <input
+                  name="title"
+                  className="form-control"
+                  value={contact.title || ''}
+                  onChange={this.onInputChange}
+                  placeholder="Senior Vice President"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Phone #</label>
+                <input
+                  name="phone"
+                  className="form-control"
+                  value={contact.phone || ''}
+                  onChange={this.onInputChange}
+                  placeholder="(435)-443-2233"
+                />
+              </div>
+
+              <div className={`form-group ${_.has(errors, 'email') && 'has-danger'}`}>
+                <label>Email</label>
+                <input
+                  name="email"
+                  className="form-control"
+                  value={contact.email || ''}
+                  onChange={this.onInputChange}
+                  placeholder="test@example.com"
+                />
+                {this.renderErrorMessages('email')}
+              </div>
+
+              <div className="form-group">
+                <label>Address</label>
+                <input
+                  name="address"
+                  className="form-control"
+                  value={contact.address || ''}
+                  onChange={this.onInputChange}
+                  placeholder="300 East Main"
+                />
+              </div>
+
+              <div className="form-group row">
+                <div className="col-xs-6">
+                  <label>City</label>
+                  <input
+                    name="city"
+                    className="form-control"
+                    value={contact.city || ''}
+                    onChange={this.onInputChange}
+                    placeholder="St. George"
+                  />
+                </div>
+                <div className="col-xs-3">
+                  <label>State</label>
+                  <input
+                    name="state"
+                    className="form-control"
+                    value={contact.state || ''}
+                    onChange={this.onInputChange}
+                    placeholder="Utah"
+                  />
+                </div>
+                <div className="col-xs-3">
+                  <label>Postal Code</label>
+                  <input
+                    name="state"
+                    className="form-control"
+                    value={contact.postal_code || ''}
+                    onChange={this.onInputChange}
+                    placeholder="84770"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-primary" onClick={this.onFormSubmit}>Add Contact</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
